f945a48d55dfe19e4a46443ea5ffbe7f0294a91e
Melissa Ridente <melissaridente3e@gmail.com>
2022-12-05 17:57:19 +0100

    Iteración #6: Mixed For...of e includes

--- ejercicios.js
+++ ejercicios.js
@@ -72,3 +72,23 @@ for (let i=0; i<placesToTravel.length; i++){
 }
 
 console.log(placesToTravel)
+
+
+/*Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato.
+ Recuerda que puedes usar la función .includes() para comprobarlo.Puedes usar este array: */
+
+ const toys = [
+  {id: 5, name: 'Buzz MyYear'}, 
+  {id: 11, name: 'Action Woman'}, 
+  {id: 23, name: 'Barbie Man'}, 
+  {id: 40, name: 'El gato con Guantes'},
+  {id: 40, name: 'El gato felix'}
+  ]
+
+  for (let value of toys){
+    if(value.name.includes('gato')){
+      toys.splice(3, 2)
+    }
+  }
+  console.log(toys)
+
