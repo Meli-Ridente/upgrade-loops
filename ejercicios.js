1fc5f3ef7cecc8f04d9aab918395252ffd5e0b80
Melissa Ridente <melissaridente3e@gmail.com>
2022-12-07 18:45:15 +0100

    Arreglos finales.

--- ejercicios.js
+++ ejercicios.js
@@ -1,15 +1,20 @@
 /*Haz un bucle y muestra por consola todos aquellos valores del array que incluyan 
 la palabra "Camiseta". Usa la función .includes de javascript. */
 
+
+console.log('EJERCICIO 1: ')
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
 for (let i=0; i<products.length; i++){
-  console.log(products[i].includes('Camiseta'));
+  if(products[i].includes('Camiseta')){
+    console.log('Estos productos llevan camiseta: ' + products[i])
+  }
 }
 
 /* Comprueba en cada uno de los usuarios que tenga al menos dos
  trimestres aprobados y añade la propiedad isApproved a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log. */
 
  
+ console.log('EJERCICIO 2: ')
 const alumns = [
   {name: 'Ana Gomes', T1: false, T2: true, T3: true}, 
   {name: 'Melissa Ridente', T1: true, T2: true, T3: true},
@@ -28,7 +33,7 @@ for (var i=0; i<alumns.length; i++){
   console.log("No está aprobado.")
 } 
 
-
+console.log('EJERCICIO 3: ')
 
 /* Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores. Puedes usar este array: */
 
@@ -40,6 +45,7 @@ for (let value of placesToTravell){
 
 /*Usa un for...in para imprimir por consola los datos del alienígena.. Puedes usar este objeto:*/
 
+console.log('EJERCICIO 4: ')
 const alien = {
   name: 'Wormuck',
   race: 'Cucusumusu',
@@ -53,6 +59,7 @@ for (let value in alien){
 
 /*Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. 
 Imprime en un console log el array. Puedes usar este array: */
+console.log('EJERCICIO 5: ')
 
 const placesToTravel = [
   {id: 5, name: 'Japan'}, 
@@ -77,6 +84,8 @@ console.log(placesToTravel)
 /*Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato.
  Recuerda que puedes usar la función .includes() para comprobarlo.Puedes usar este array: */
 
+ console.log('EJERCICIO 6: ')
+
  const toys = [
   {id: 5, name: 'Buzz MyYear'}, 
   {id: 11, name: 'Action Woman'}, 
@@ -92,7 +101,7 @@ console.log(placesToTravel)
   }
   console.log(toys)
 
-
+  console.log('EJERCICIO 7: ')
   /*Usa un bucle for...of para recorrer todos los juguetes 
   y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:*/
 
